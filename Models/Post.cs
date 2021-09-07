b6355993cff60d98fc7c2101f955cc1dca1dc945
jdshez <jd_shez@protonmail.com>
2021-09-07 23:59:43 +0100

    Naming convention update to models

--- Models/Post.cs
+++ Models/Post.cs
@@ -14,7 +14,7 @@ namespace TheBlogProject.Models
     {
         public int Id { get; set; }
         public int BlogId { get; set; }
-        public string AuthorId { get; set; }
+        public string BlogUserId { get; set; }
 
         [Required]
         [StringLength(75, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long", MinimumLength = 2)]
@@ -48,7 +48,7 @@ namespace TheBlogProject.Models
         //Navigation Properties
 
         public virtual Blog Blog { get; set; }
-        public virtual BlogUser Author { get; set; }
+        public virtual BlogUser BlogUser { get; set; }
 
         public virtual ICollection<Tag> Tags { get; set; } = new HashSet<Tag>();
         public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
